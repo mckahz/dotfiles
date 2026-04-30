affd430fa89f31c01324d5672d6a04533a869a42
mckahz <fletchusmaximus@gmail.com>
2026-04-30 23:19:48 +1000

    added desktop

---
+++ nixos/hosts/desktop/configuration.nix
@@ -0,0 +1,21 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "desktop";
+
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/nvidia.nix
+  ];
+
+  keyboard = {
+    enable = true;
+    path = ./keyboard.kbd;
+  };  
+
+  system.stateVersion = "25.11";
+}
