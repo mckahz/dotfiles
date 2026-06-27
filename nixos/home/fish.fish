47ade99739a446606c76001a0b7dc72ac469a801
mckahz <fletchusmaximus@gmail.com>
2026-06-28 02:34:30 +1000

    configured prompt

--- nixos/home/fish.fish
+++
@@ -1,6 +0,0 @@
-set fish_greeting # Disable greeting
-set fish_vi_key_bindings true
-
-# Set prompt
-# tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Few icons' --transient=No
-set tide_left_prompt_items pwd
--- nixos/home/fish.nix
+++ nixos/home/fish.nix
@@ -1,16 +1,21 @@
 { pkgs, ... }:
-let
-  enable = true;
-in
 {
   programs.fish = {
-    inherit enable;
+    enable = true;
 
     shellAbbrs = {
       nv = "nvim";
     };
 
-    interactiveShellInit = builtins.readFile ./fish.fish;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+      fish_vi_key_bindings
+
+      set -g tide_left_prompt_items vi_mode pwd newline character
+      set -g tide_right_prompt_items status cmd_duration jobs node python rustc java php pulumi ruby go gcloud kubectl distrobox toolbox terraform aws nix_shell crystal elixir zig
+
+      tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Few icons' --transient=No
+    '';
 
     plugins =
       let
