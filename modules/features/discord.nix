40406939ea0abdab2354f46c152deabcf4de97b9
mckahz <fletchusmaximus@gmail.com>
2026-07-20 13:02:59 +0000

    added default discord client

--- modules/features/discord.nix
+++ modules/features/discord.nix
@@ -23,7 +23,8 @@
           user = config.home.username;
 
           vesktop.enable = true;
-          #removeWarningPopup = true;
+          discord.vencord.enable = true; # Standard Vencord
+          #discord.silenceNoModClientWarning = true;
           #discord.vencord.enable = true; # Standard Vencord
         };
       };
